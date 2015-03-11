c529354ea82144f1ad414457dc57be3febe09d9d
Chad Walker <chad@chad-cat-lore-eddie.com>
2015-03-11 17:27:35 -0500

    dox for node 0.12.0

---
+++ doxygen/html/search/variables_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['kframedropperframesize',['kFrameDropperFrameSize',['../classv8_1_1internal_1_1_live_edit.html#ae7c4b85cb933c090d52eadc6b59d1ed6',1,'v8::internal::LiveEdit']]]
+];
